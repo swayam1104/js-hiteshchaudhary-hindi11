25f48dff6e73780083ca50864afdd4faf9597490
swayam1104 <shekharswayam64@gmail.com>
2026-09-05 09:19:09 +0000

    stack n heaps

--- .devcontainer/01_basics/datatypes-summery.js
+++ .devcontainer/01_basics/datatypes-summery.js
@@ -5,14 +5,38 @@
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id);
-console.log(anotherId);
-console.log(id == anotherId);
+// console.log(id);
+// console.log(anotherId);
+// console.log(id == anotherId);
 
 const scoreValue = 3492358928942;
-console.log(typeof scoreValure);
-console.log(typeof null);
+// console.log(typeof scoreValure);
+// console.log(typeof null);
 
 
 //reference types
 //array, objects, functions 
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack(Primitive), Heap(Non Primitive)
+
+let myName = "Swayam Shekhar"
+let homeName = myName
+console.log(homeName);
+homeName = "Amull"
+console.log(myName);
+console.log(homeName);
+
+
+//HEAPS properties
+let userOne = {
+    name: "Swayam",
+    usn: 214
+}
+let userTwo = userOne;
+userTwo.usn = 213
+console.log(userOne.usn);
+console.log(userTwo.usn);
