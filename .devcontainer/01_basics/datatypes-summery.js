ba0f8f4931567bde642f7c94630381d37cbedf58
swayam1104 <shekharswayam64@gmail.com>
2026-09-05 08:53:36 +0000

    datatypes summery

---
+++ .devcontainer/01_basics/datatypes-summery.js
@@ -0,0 +1,18 @@
+//primitive
+// 7 types: String, Number, Boolean, null, undefined, symbol, bigint
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id);
+console.log(anotherId);
+console.log(id == anotherId);
+
+const scoreValue = 3492358928942;
+console.log(typeof scoreValure);
+console.log(typeof null);
+
+
+//reference types
+//array, objects, functions 
