b900b425288b9e8c0b8268a7ac5cf11d40fd2368
swayam1104 <shekharswayam64@gmail.com>
2026-09-05 07:20:39 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,10 @@
+let score = "33";
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+//1 -> true
+// 0 -> falswe
+// "swayam" -> true
+// "" -> false
