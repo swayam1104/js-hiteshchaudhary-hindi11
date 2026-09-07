d8c32f2ff025c060de50ac182d1ab18b98839c29
swayam1104 <shekharswayam64@gmail.com>
2026-09-07 06:38:33 +0000

    object ho gya ab

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,35 @@
+//const tinderUser = new Object();
+const tinderUser = {};
+tinderUser.id = "123swa";
+tinderUser.name = "Swayam";
+tinderUser.isLoggedIn = false;
+
+console.log(tinderUser);
+
+const ob1 = {1:"a", 2: "b"};
+const ob2 = {3:"c", 4: "d"};
+const ob3 = { ob1, ob2 };
+console.log(ob3);
+
+const ob4 = Object.assign({},ob1, ob2);
+console.log(ob4);
+
+const ob5 = {...ob1, ...ob2};
+console.log(ob5);
+
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn')) // batayega ki ye properrty h ya nhi
+
+const course = {
+    courseName: "JavaScript",
+    price: 999,
+    courseInstructor: "Amull"
+}
+const {courseInstructor : teacher} = course;
+console.log({teacher});
+
+const navbar = () => {
+
+}
+navbar(company =  )
